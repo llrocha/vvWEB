6f9c4216ac50cc30a05ea295850ef478f066b8f0
Leonardo Rocha <leonardolpi@gmail.com>
2017-07-28 16:31:34 -0300

    vv - version verify, with django framework

---
+++ vv/vvPromax/views.py
@@ -0,0 +1,73 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpRequest, HttpResponse
+from datetime import datetime
+from vv import settings
+
+def home(request):
+    """Renders the home page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'vvPromax/index.html',
+        {
+            'menu':'',
+            'appname':'vvPromax',
+            'title':'Home Page',
+            'year':datetime.now().year,
+        }
+    )
+
+def comparar(request):
+    """Renders the comparar page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'vvPromax/comparar.html',
+        {
+            'menu':'comparar',
+            'appname':'vvPromax',
+            'title':'Home Page',
+            'year':datetime.now().year,
+        }
+    )
+
+def verificar(request):
+    """Renders the verificar page."""
+    return render(
+        request,
+        'vvPromax/verificar.html',
+        {
+            'menu':'verificar',
+            'appname':'vvPromax',
+            'title':'Home Page',
+            'year':datetime.now().year,
+        }
+    )
+
+def contato(request):
+    """Renders the contato page."""
+    return render(
+        request,
+        'vvPromax/contato.html',
+        {
+            'menu':'contato',
+            'appname':'vvPromax',
+            'title':'Home Page',
+            'year':datetime.now().year,
+        }
+    )
+
+def sobre(request):
+    """Renders the sobre page."""
+    return render(
+        request,
+        'vvPromax/sobre.html',
+        {
+            'menu':'sobre',
+            'appname':'vvPromax',
+            'title':'Home Page',
+            'year':datetime.now().year,
+        }
+    )
